8591da6065abddb5049634886dcd80a228946661
Alessia Orlando <aorlando@lxplus9129.cern.ch>
2025-07-25 19:37:29 +0200

    Data exploration

--- muonDIS_scifi.py
+++ muonDIS_scifi.py
@@ -4,10 +4,10 @@ import SndlhcGeo
 from os.path import exists
 
 geo = SndlhcGeo.GeoInterface("/eos/experiment/sndlhc/users/fmei/muons_with_box/geant4_output/geofile_full.muonDIS-TGeant4-muonDis_0.root")
-scifi = geo.snd_geo.Scifi # SciFi geometry
-nscifi = scifi.nscifi
+scifi = geo.snd_geo.Scifi  # SciFi geometry
+nscifi = scifi.nscifi  # number of SciFi stations
 file_path = "/eos/experiment/sndlhc/users/fmei/muons_with_box/geant4_output/sndLHC.muonDIS-TGeant4-muonDis_"
-cbmsim = ROOT.TChain("cbmsim")
+cbmsim = ROOT.TChain("cbmsim")  # chain TTrees from different files
 
 n_files_to_read = 5
 for i in range (n_files_to_read):
@@ -17,12 +17,9 @@ for i in range (n_files_to_read):
     this_read = cbmsim.Add(file_name)
 
 # Detector coordinates
-x_min = -80
-x_max = 5
-y_min = 5
-y_max = 75
-z_min = 270
-z_max = 580
+x_min, x_max = -80, 5
+y_min, y_max = 5, 75
+z_min, z_max = 270, 580
 x_range = x_max - x_min
 y_range = y_max - y_min
 z_range = z_max - z_min
@@ -40,24 +37,23 @@ h_scifi_hits_stations = ROOT.TH1D("h_scifi_hits_stations", "Scifi hits per stati
 # QDC
 h_scifi_qdc = ROOT.TH1D("h_scifi_qdc", "SciFi signal; QDC", 200, -2, 20)
 
-# ENERGY LOSS
-h_scifi_energy_loss = ROOT.TH1D("h_scifi_energy_loss", "Total energy loss in SciFi; E[MeV]", 200, 0, 20)
 
-
-for i_event, event in enumerate(cbmsim):
+for i_event, event in enumerate(cbmsim):  # loop over events
     if len(cbmsim.MCTrack) == 0: continue
     n_scifi_hits = len(event.Digi_ScifiHits)
-    if n_scifi_hits >= 1:
-        incoming_mu = event.MCTrack[0]
+    if n_scifi_hits >= 1:  # select events with at least one hit in SciFi
+        incoming_mu = event.MCTrack[0]  # incoming muon track
 
+        # coordinates of the MCTrack start
         start_x = incoming_mu.GetStartX()
         start_y = incoming_mu.GetStartY()
         start_z = incoming_mu.GetStartZ()
 
+        # starting stations
         starting_station = -1
         for i in range(nscifi):
             z_pos = getattr(scifi, f"Ypos{i}")
-            if i == 0:
+            if i == 0:  # first SciFi station
                 if z_pos - 10 < start_z < z_pos:
                     starting_station = i
                     break
@@ -69,16 +65,11 @@ for i_event, event in enumerate(cbmsim):
         if starting_station != -1:
             h_scifi_starting_station.Fill(starting_station)
         
-        for scifihit in event.Digi_ScifiHits:
+        for scifihit in event.Digi_ScifiHits:  # loop over digitized hits
             station = scifihit.GetStation()
             h_scifi_hits_stations.Fill(station)
-            if scifihit.GetSignal() > -999: # hit SiPM
+            if scifihit.GetSignal() > -999:  # hit SiPM
                 h_scifi_qdc.Fill(scifihit.GetSignal())
-
-        scifi_energy_loss = 0
-        for scifipoint in event.ScifiPoint:
-            scifipoint_e_loss = scifipoint.GetEnergyLoss() * 1000
-            scifi_energy_loss += scifipoint_e_loss
 
         h_incoming_x.Fill(incoming_mu.GetStartX())        
         h_incoming_y.Fill(incoming_mu.GetStartY())
@@ -86,7 +77,6 @@ for i_event, event in enumerate(cbmsim):
 
         h_scifi_hits.Fill(n_scifi_hits)
 
-        h_scifi_energy_loss.Fill(scifi_energy_loss)
 
 c_incoming_x = ROOT.TCanvas("c_incoming_x")
 h_incoming_x.Draw()
@@ -116,10 +106,6 @@ c_scifi_qdc = ROOT.TCanvas("c_scifi_qdc")
 h_scifi_qdc.Draw()
 c_scifi_qdc.Draw()
 
-c_scifi_energy_loss = ROOT.TCanvas("c_scifi_energy_loss")
-h_scifi_energy_loss.Draw()
-c_scifi_energy_loss.Draw()
-
 output = ROOT.TFile("muonDIS_scifi_plots.root", "RECREATE")
 
 h_incoming_x.Write()
@@ -130,7 +116,5 @@ h_scifi_starting_station.Write()
 h_scifi_hits.Write()
 h_scifi_hits_stations.Write()
 h_scifi_qdc.Write()
-
-h_scifi_energy_loss.Write()
 
 output.Close()
